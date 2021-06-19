8f3a29a99f29affbc8e03169a0c59cb3f2c3f596
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2021-06-19 13:52:03 -0400

    Basic Header with Login created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,28 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AuthModule } from '@auth0/auth0-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
-// component imports
-import { AppComponent } from './app.component';
-import { AuthButtonComponent } from './components/auth-button/auth-button.component';
+// service imports
+import {AuthService} from '@auth0/auth0-angular';
 
 // environment import
 import { environment } from '../environments/environment';
 
+// component imports
+import { AppComponent } from './app.component';
+import { AuthButtonComponent } from './components/auth-button/auth-button.component';
+import { HeaderComponent } from './components/header/header.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    AuthButtonComponent],
+    AuthButtonComponent,
+    HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
     AuthModule.forRoot({
       domain: environment.domain,
       clientId: environment.clientId
     })
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
